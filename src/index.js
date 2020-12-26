42c072146a3a3454e7ffbc7da396d0943caf60cd
Me <mergorgec@gmail.com>
2020-12-26 10:33:12 +0100

    add firebase config and context api --connected

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import Firebase, { FirebaseContext } from './components/Firebase';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <FirebaseContext.Provider value={new Firebase()}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    ,
+  </FirebaseContext.Provider>,
+
   document.getElementById('root'),
 );
 
